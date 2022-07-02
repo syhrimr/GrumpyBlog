1cf627e7f9bd41a88ae5ee5a44847e1556326612
syhrimr <syahri.maulana@gmail.com>
2022-07-02 15:24:54 +0000

    add a new custom header component

---
+++ src/components/gp-header/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+    <nav>
+      <ul class="gp-header-menu">
+        <li v-for="{ path, name } in routes">
+          <RouterLink
+            :to="path"
+            >{{ name }}</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </section>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {
+  name: "Header",
+  props: {
+    routes: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.router-link-active {
+  background-color: #04AA6D;
+}
+</style>
